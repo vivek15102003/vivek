d14103c6f27aab7f887c99291cebba9207ddda84
vivek15102003 <vivekgowdababbu2@gmail.com>
2025-03-25 12:05:26 +0530

    day of push

---
+++ Patient .java
@@ -0,0 +1,8 @@
+class Chandru{
+public static void main (String abc[]){
+int patientId;
+char gender='A'+'8';
+System.out.println("the paitent id is "+patientId);
+System.out.println("the patient gender is "+gender);
+}
+}
